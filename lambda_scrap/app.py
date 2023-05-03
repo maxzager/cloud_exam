b9aebbfdf3f3a8bc974fe1b656802a669b4893d2
maxzager <maxgreminger@hotmail.com>
2023-05-04 00:52:01 +0200

    delete flask app

--- lambda_scrap/app.py
+++ lambda_scrap/app.py
@@ -1,17 +1,9 @@
-import os, json
-from flask import Flask
 from lambda_scrap import run_web_scraping
 
-app = Flask(__name__)
-@app.route('/')
 def lambda_handler():
     run_web_scraping()
-    return {
-        
-        'statusCode': 200,
-        'body': json.dumps('Success')
-    }
+    return "OK"
     
 if __name__ == '__main__':
-    app.run(os.getenv("HOST", "0.0.0.0"), port=os.getenv("PORT", 8080))
+    lambda_handler()
 
